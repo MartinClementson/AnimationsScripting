0568005236835e5fffb8ed7f13de3081e7d82c8b
MartinClementson <maik14@H473-0015.students.ad.bth.se>
2016-02-26 18:45:37 +0100

    Things work awesomely

--- MocapScript.py
+++ MocapScript.py
@@ -3,8 +3,8 @@ import time
 
 import sys
 
-#sys.path.insert(0,'//storage10.student.bth.se/students/20142/maik14/AnimationsScripting/')
-sys.path.insert(0,'C:/Users/Martin/Desktop/AnimationsScripting/')
+sys.path.insert(0,'//storage10.student.bth.se/students/20142/maik14/AnimationsScripting/')
+#sys.path.insert(0,'C:/Users/Martin/Desktop/AnimationsScripting/')
 
 
 try:
@@ -13,18 +13,24 @@ except:
     import transferFunctions as tf
 
     
-src = ['iPi:Hip', 'iPi:LThigh', 'iPi:LShin', 'iPi:LFoot', 'iPi:LToe', 'iPi:LToeEnd', 'iPi:RThigh', 'iPi:RShin', 'iPi:RFoot', 'iPi:RToe', 'iPi:RToeEnd', 'iPi:LowerSpine', 'iPi:MiddleSpine', 'iPi:Chest', 'iPi:LClavicle', 'iPi:LShoulder', 'iPi:LForearm', 'iPi:LHand', 'iPi:LHandEnd', 'iPi:RClavicle', 'iPi:RShoulder', 'iPi:RForearm', 'iPi:RHand', 'iPi:RHandEnd', 'iPi:Neck', 'iPi:Head', 'iPi:HeadEnd']
+#src = ['iPi:Hip', 'iPi:LThigh', 'iPi:LShin', 'iPi:LFoot', 'iPi:LToe', 'iPi:LToeEnd', 'iPi:RThigh', 'iPi:RShin', 'iPi:RFoot', 'iPi:RToe', 'iPi:RToeEnd', 'iPi:LowerSpine', 'iPi:MiddleSpine', 'iPi:Chest', 'iPi:LClavicle', 'iPi:LShoulder', 'iPi:LForearm', 'iPi:LHand', 'iPi:LHandEnd', 'iPi:RClavicle', 'iPi:RShoulder', 'iPi:RForearm', 'iPi:RHand', 'iPi:RHandEnd', 'iPi:Neck', 'iPi:Head', 'iPi:HeadEnd']
 #src = pm.PyNode('iPi:Hip')
 #src = pm.PyNode('iPi:RThigh')
 #src = pm.PyNode('iPi:RShin')
 #src = pm.PyNode('iPi:RFoot')
 
-dest = ['HIKCharacterNode1_Hips', 'HIKCharacterNode1_LeftUpLeg', 'HIKCharacterNode1_LeftLeg', 'HIKCharacterNode1_LeftFoot', 'HIKCharacterNode1_LeftFootMiddle1', 'HIKCharacterNode1_LeftFootMiddle2', 'HIKCharacterNode1_RightUpLeg', 'HIKCharacterNode1_RightLeg', 'HIKCharacterNode1_RightFoot', 'HIKCharacterNode1_RightFootMiddle1', 'HIKCharacterNode1_RightFootMiddle2', 'HIKCharacterNode1_Spine', 'HIKCharacterNode1_Spine1', 'HIKCharacterNode1_Spine2', 'HIKCharacterNode1_LeftShoulder', 'HIKCharacterNode1_LeftArm', 'HIKCharacterNode1_LeftForeArm', 'HIKCharacterNode1_LeftHand', 'HIKCharacterNode1_LeftFingerBase', 'HIKCharacterNode1_RightShoulder', 'HIKCharacterNode1_RightArm', 'HIKCharacterNode1_RightForeArm', 'HIKCharacterNode1_RightHand', 'HIKCharacterNode1_RightFingerBase', 'HIKCharacterNode1_Neck', 'HIKCharacterNode1_Neck1', 'HIKCharacterNode1_Head']
+
+#src = pm.PyNode('iPi:LThigh')
+#src = pm.PyNode('iPi:LShin')
+#dest = ['HIKCharacterNode1_Hips', 'HIKCharacterNode1_LeftUpLeg', 'HIKCharacterNode1_LeftLeg', 'HIKCharacterNode1_LeftFoot', 'HIKCharacterNode1_LeftFootMiddle1', 'HIKCharacterNode1_LeftFootMiddle2', 'HIKCharacterNode1_RightUpLeg', 'HIKCharacterNode1_RightLeg', 'HIKCharacterNode1_RightFoot', 'HIKCharacterNode1_RightFootMiddle1', 'HIKCharacterNode1_RightFootMiddle2', 'HIKCharacterNode1_Spine', 'HIKCharacterNode1_Spine1', 'HIKCharacterNode1_Spine2', 'HIKCharacterNode1_LeftShoulder', 'HIKCharacterNode1_LeftArm', 'HIKCharacterNode1_LeftForeArm', 'HIKCharacterNode1_LeftHand', 'HIKCharacterNode1_LeftFingerBase', 'HIKCharacterNode1_RightShoulder', 'HIKCharacterNode1_RightArm', 'HIKCharacterNode1_RightForeArm', 'HIKCharacterNode1_RightHand', 'HIKCharacterNode1_RightFingerBase', 'HIKCharacterNode1_Neck', 'HIKCharacterNode1_Neck1', 'HIKCharacterNode1_Head']
 
 #destRoot = pm.PyNode('HIKCharacterNode1_Hips')
 #destRoot = pm.PyNode('HIKCharacterNode1_RightUpLeg')
 #destRoot = pm.PyNode('HIKCharacterNode1_RightLeg')
 #destRoot = pm.PyNode('HIKCharacterNode1_RightFoot')
+
+#destRoot = pm.PyNode('HIKCharacterNode1_LeftUpLeg')
+#destRoot = pm.PyNode('HIKCharacterNode1_LeftLeg')
 print "MOCAP SCRIPT LOADED!!"
 #hej = tf.printChildren(src, 0)
 #tf.transferAnimation(src, dest)
--- UIController.py
+++ UIController.py
@@ -27,19 +27,19 @@ def testThread(selection,currentJoint):
     on = True
     while on:
 
-	checkJoint = pm.ls(sl = True, type = 'joint')
+        checkJoint = pm.ls(sl = True, type = 'joint')
 
-	if checkJoint:
+        if checkJoint:
 
-	    if checkJoint != currentJoint:
+            if checkJoint != currentJoint:
 
-		currentJoint = checkJoint[0]
-		on = False
+                currentJoint = checkJoint[0]
+                on = False
 
 
 
-	print "hej"
-	time.sleep(2)
+        print "hej"
+        time.sleep(2)
 
 
 
@@ -47,164 +47,306 @@ class myUIController(QObject):
 
     def __init__(self, ui):
 
-	Obj = QObject.__init__(self)
+        Obj = QObject.__init__(self)
 
-	#Obj = QObject
-	#Obj = QObject.thread(self)
-	print Obj
-	self.ui = ui
-	   
-
-	#self.deleteLater()
-	#self.ui.setWindowFlags(Qt.Window)
-
-	#TransferButton
-	ui.transferButton.clicked.connect(self.transferButtonClicked)
+        #Obj = QObject
+        #Obj = QObject.thread(self)
+        print Obj
+        self.ui = ui
 
 
-	#Left Buttons
-	ui.leftUpButton.clicked.connect(self.leftUpButtonClicked)
-	ui.leftDownButton.clicked.connect(self.leftDownButtonClicked)
-	ui.leftDeleteButton.clicked.connect(self.leftDeleteButtonClicked)
+        #self.deleteLater()
+        #self.ui.setWindowFlags(Qt.Window)
 
-	#Right Buttons
-	ui.rightUpButton.clicked.connect(self.rightUpButtonClicked)
-	ui.rightDownButton.clicked.connect(self.rightDownButtonClicked)
-	ui.rightDeleteButton.clicked.connect(self.rightDeleteButtonClicked)	
+        #TransferButton
+        #ui.transferButton.setEnabled(False)
+        ui.transferButton.clicked.connect(self.transferButtonClicked)
+        
+        #warning message
+        ui.warningLabel.setText(' ')
 
+        #ui.warningLabel.setText('There is not an even number of joints')
+        
+        #Left Buttons
+        ui.leftUpButton.clicked.connect(self.leftUpButtonClicked)
+        ui.leftDownButton.clicked.connect(self.leftDownButtonClicked)
+        ui.leftDeleteButton.clicked.connect(self.leftDeleteButtonClicked)
+        ui.leftLoadButton.clicked.connect(self.loadSourceJoints)
+        ui.SourceRootJoint.returnPressed.connect(self.loadSourceJointsFromString)
+        
+       
 
-	#Left list
-
-	shapes = ['Square','Triangle','Arrow','Circle']
-	ui.SourceList.clear()
-	ui.SourceList.addItems(shapes)
-	ui.SourceList.setCurrentRow(0)
-	#ui.selectedShape.setText('Square')
-	#i.selectedShape.displayText()	
-
-	ui.SourceList.itemSelectionChanged.connect(self.SourcelistItemChanged)
-
-
-	#Right list
-
-	ui.TargetList.clear()
-	ui.TargetList.addItems(shapes)
-	ui.TargetList.setCurrentRow(0)
-	#ui.selectedShape.setText('Square')
-	#i.selectedShape.displayText()	
-
-	ui.TargetList.itemSelectionChanged.connect(self.TargetlistItemChanged)    
+        #Right Buttons
+        ui.rightUpButton.clicked.connect(self.rightUpButtonClicked)
+        ui.rightDownButton.clicked.connect(self.rightDownButtonClicked)
+        ui.rightDeleteButton.clicked.connect(self.rightDeleteButtonClicked)
+        ui.rightLoadButton.clicked.connect(self.loadTargetJoints)
+        ui.TargetRootJoint.returnPressed.connect(self.loadTargetJointsFromString)
 
 
+        #Left list
+
+        shapes = ['Square','Triangle','Arrow','Circle']
+        ui.SourceList.clear()
+        ui.SourceList.addItems(shapes)
+        ui.SourceList.setCurrentRow(0)
+        
+        #ui.SourceRootJoint.keyPressEvent()
+        
+        #i.selectedShape.displayText()	
+
+        ui.SourceList.itemSelectionChanged.connect(self.SourcelistItemChanged)
+
+
+        #Right list
+
+        ui.TargetList.clear()
+        ui.TargetList.addItems(shapes)
+        ui.TargetList.setCurrentRow(0)
+        #ui.selectedShape.setText('Square')
+        #i.selectedShape.displayText()	
+
+        ui.TargetList.itemSelectionChanged.connect(self.TargetlistItemChanged)    
 
 
 
-	self.jointSel = pm.ls(sl = True, type = 'joint')
-	#self.thread = start_new_thread(testThread,(self.ui,self.jointSel[0]))
-
-	#try:
-	    #  ui.jointLabel.setText(str(self.jointSel[0]))
-	#except Exception, e:
-	    #  ui.jointLabel.setText("None")
 
 
-	# ui.image.setPixmap(self.ui.path + "\icons\Square.jpg")
-	#ui.expMat.stateChanged.connect(self.expMatChanged)
-	#print ui.QMouseEvent
-	#self.mousePressEvent(obj)
-	#self.obj.event()
-	#mouseFilter = print("HEJ")
-	#ui.installEventFilter(mouseFilter)
+        self.jointSel = pm.ls(sl = True, type = 'joint')
+        #self.thread = start_new_thread(testThread,(self.ui,self.jointSel[0]))
 
-	self.windowActive = True
-	self.ui.show() 
+        #try:
+            #  ui.jointLabel.setText(str(self.jointSel[0]))
+        #except Exception, e:
+            #  ui.jointLabel.setText("None")
 
 
-    def __del__(self):
-	del self.ui
-	del self
+        # ui.image.setPixmap(self.ui.path + "\icons\Square.jpg")
+        #ui.expMat.stateChanged.connect(self.expMatChanged)
+        #print ui.QMouseEvent
+        #self.mousePressEvent(obj)
+        #self.obj.event()
+        #mouseFilter = print("HEJ")
+        #ui.installEventFilter(mouseFilter)
+
+        self.windowActive = True
+        self.ui.show() 
+
+
+    #def __del__(self):
+      #  del self.ui
+      #  del self
+      
     def showUI(self):
-	self.ui.show()
-	self.windowActive = True
-	#start_new_thread(testThread,(self.windowActive))
+        self.ui.show()
+        self.windowActive = True
+        #start_new_thread(testThread,(self.windowActive))
 
     def hideUI(self):
-	self.windowActive = False
-	self.ui.close()
-	del self
+        self.windowActive = False
+        self.ui.close()
+        del self
 
     def test(self):
-	print "Threading!"
+        print "Threading!"
 
 
 
     def mousePressEvent(self, event):
-		# Panning
-	if event.event():
-	    print "mousePress ok!"
+                # Panning
+        if event.event():
+            print "mousePress ok!"
+    
+    def loadSourceJoints(self):
+        self.loadJoints(self.ui.SourceList)
+        self.ui.L_jointAmount.setText(str(self.ui.SourceList.count()))
+        self.compareJointCount()
+        self.ui.SourceList.setCurrentRow(0)
+    
+    def loadSourceJointsFromString(self):
+        #print "LOAD FROM STRING"
+        stringToLoad = self.ui.SourceRootJoint.text()
+        self.loadJointsFromString(self.ui.SourceList,stringToLoad)
+        self.ui.L_jointAmount.setText(str(self.ui.SourceList.count()))
+        self.compareJointCount()
+        self.ui.TargetList.setCurrentRow(0)        
+        
+    
+    def loadTargetJointsFromString(self):
+        stringToLoad = self.ui.TargetRootJoint.text()
+        self.loadJointsFromString(self.ui.TargetList,stringToLoad)
+        self.ui.R_jointAmount.setText(str(self.ui.TargetList.count()))
+        self.compareJointCount()
+        self.ui.TargetList.setCurrentRow(0)        
+    
+    def loadTargetJoints(self):
+        self.loadJoints(self.ui.TargetList)
+        self.ui.R_jointAmount.setText(str(self.ui.TargetList.count()))
+        self.compareJointCount()
+        self.ui.TargetList.setCurrentRow(0)
+            
+    def loadJoints(self, listWidget):
+        print "LOAD JOINTS CAPN!"
+        listWidget.clear()
+        root = pm.ls(sl = True, type = 'joint')
+        relatives = root[0].listRelatives(ad = True) #ad =all decendants
+        relatives.reverse()
+        
+        listItems = [str(root[0])]
+        for each_joint in relatives:
+            listItems.append(str(each_joint))
+        listWidget.addItems(listItems)
+    
+    
+    def loadJointsFromString(self,listWidget,string):
+        
+        
+        
+        try:
+            root = pm.PyNode(string)
+            listWidget.clear()
+            relatives = root.listRelatives(ad = True) #ad =all decendants
+            relatives.reverse()
+            
+            listItems = [str(root)]
+            for each_joint in relatives:
+                listItems.append(str(each_joint))
+            listWidget.addItems(listItems) 
+        except:
+            print "No match found"
+        
 
 
     def leftUpButtonClicked(self):
-	print "Left UP"
-	#self.windowActive = False
-	#self.ui.close()
+        print "Left UP"
+        self.moveListItemUp(self.ui.SourceList)
+        
+     
 
     def leftDownButtonClicked(self):
-	print "Left DOWN"
+        print "Left DOWN"
+        self.moveListItemDown(self.ui.SourceList)
+        
 
 
     def leftDeleteButtonClicked(self):
-	toDelete = self.ui.SourceList.selectedItems()
+        if self.ui.SourceList.count():
+            
+            toDelete = self.ui.SourceList.selectedItems()
 
-	self.ui.SourceList.takeItem(self.ui.SourceList.row(toDelete[0]))	
-	print "Left Delete"
+            self.ui.SourceList.takeItem(self.ui.SourceList.row(toDelete[0]))	
+            print "Left Delete"
+            self.compareJointCount()
+            self.ui.L_jointAmount.setText(str(self.ui.SourceList.count()))
+        else:
+            print "There is nothing to delete"
 
 
 
 
     def rightUpButtonClicked(self):
-	print "right UP"
-	#self.windowActive = False
-	#self.ui.close()
+        self.moveListItemUp(self.ui.TargetList)
+        print "right UP"
+        
 
     def rightDownButtonClicked(self):
+        self.moveListItemDown(self.ui.TargetList)
 
-	print "right DOWN"
+        print "right DOWN"
 
 
     def rightDeleteButtonClicked(self):
-	if self.ui.TargetList.count():
+        if self.ui.TargetList.count():
 
-	    toDelete = self.ui.TargetList.selectedItems()
+            toDelete = self.ui.TargetList.selectedItems()
 
-	    self.ui.TargetList.takeItem(self.ui.TargetList.row(toDelete[0]))
-	    print "right Delete"
-	else:
-	    print"NOTHING TO DELETE"
+            self.ui.TargetList.takeItem(self.ui.TargetList.row(toDelete[0]))
+            self.compareJointCount()
+            self.ui.R_jointAmount.setText(str(self.ui.TargetList.count()))
+            print "right Delete"
+        else:
+            print"NOTHING TO DELETE"
 
 
 
 
     def transferButtonClicked(self):
-	print "Transferring"
+        
+        loop = self.ui.SourceList.count()
+        
+        for x in range(loop):
+            temp = self.ui.SourceList.item(x)
+            
+            print str(temp.text())
 
 
 
 
 
 
-	#curvez.setRotation(joint._getRotation('transform'),'transform')
-	print 'create button is clicked'
+        #curvez.setRotation(joint._getRotation('transform'),'transform')
+        print 'create button is clicked'
 
     def parentButtonClicked(self):
-	print 'parent button is clicked'
+        print 'parent button is clicked'
 
 
     def SourcelistItemChanged(self):
-	print "source list item changed"
+        print "source list item changed"
 
     def TargetlistItemChanged(self):
-	print "Target list item changed"
+        print "Target list item changed"
+        
+        
+        
+    def moveListItemUp(self,listWidget):
+        print "Left UP"
+        if listWidget.currentRow() > 0:
+            
+            if listWidget.currentRow() == (listWidget.count() -1): #If it's the one furthest down, do this 
+                
+                itemToMove = listWidget.takeItem(listWidget.currentRow())
+                                
+                #self.ui.SourceList.setCurrentRow((self.ui.SourceList.currentRow() -1))
+                listWidget.insertItem((listWidget.currentRow()),itemToMove)
+                listWidget.setCurrentRow((listWidget.currentRow() -1))
+                print "moving bottom up"
+                
+                
+            else:
+                itemToMove = listWidget.takeItem(listWidget.currentRow())
+                
+                #self.ui.SourceList.setCurrentRow((self.ui.SourceList.currentRow() -1))
+                listWidget.insertItem((listWidget.currentRow()-1),itemToMove)
+                listWidget.setCurrentRow((listWidget.currentRow() -2))
+                #self.windowActive = False
+                #self.ui.close()
+        else:
+            print"You're at the top!"        
+    
+    def moveListItemDown(self,listWidget):
+        if listWidget.currentRow() < (listWidget.count() -1):
+                   
+                     
+            itemToMove = listWidget.takeItem(listWidget.currentRow())
+                   
+            listWidget.insertItem((listWidget.currentRow()+1),itemToMove)
+            listWidget.setCurrentRow((listWidget.currentRow() +1))
+                
+        else:
+            print "AT THE BOTTOM"
+    
+    
+    def compareJointCount(self):
+        
+        if self.ui.SourceList.count() != self.ui.TargetList.count():
+            
+            self.ui.warningLabel.setText('There is not an even number of joints')
+            self.ui.transferButton.setEnabled(False)
+        else:
+            self.ui.warningLabel.setText(' ')
+            self.ui.transferButton.setEnabled(True)
+            
 
 
 
