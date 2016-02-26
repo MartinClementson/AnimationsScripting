1f67c76b7d5e6ee05c6f000bd4b6198ead6765dc
MartinClementson <maik14@H473-0015.students.ad.bth.se>
2016-02-26 18:47:08 +0100

    aaaa

--- loadUIinMaya.py
+++ loadUIinMaya.py
@@ -3,28 +3,28 @@ import sys
 import pymel.core as pm
 import time
 
-#sys.path.insert(0,'//storage10.student.bth.se/students/20142/maik14/AnimationsScripting/')
-sys.path.insert(0,'C:/Users/Martin/Desktop/AnimationsScripting/')
+sys.path.insert(0,'//storage10.student.bth.se/students/20142/maik14/AnimationsScripting/')
+#sys.path.insert(0,'C:/Users/Martin/Desktop/AnimationsScripting/')
 
 
 import loadXMLUI
+
+
+gui = loadXMLUI.loadUI("transferUI.ui")
+#iPi:Hip
+
 try:
     muic = reload(muic)
 except:
 		
 	import UIController as muic
 
-
-
-gui = loadXMLUI.loadUI("transferUI.ui")
-
-
-
-
 controllerz = muic.myUIController(gui)
+
+
 controllerz.ui.show()
 controllerz.__del__()
-    
+gui.
 print controllerz
 del controllerz
 controllerz.ui.show()
